e622e4d3cfe089d6e5ad379773af5c912d248b08
Pizzosaure <melissafranca3@gmail.com>
2024-01-16 15:24:33 +0100

    style contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,26 +4,31 @@ function ContactPage() {
   return (
     <>
       <div className="contact-container">
-        <h1>La Patisserie du Dauphin arc-en-ciel </h1>
-        <p>
-          Un mariage raté? Un craquage pendant votre régime? Une tentative pour
-          impressionner votre belle-mère? Continuez à chercher, cette patisserie
-          n'est qu'imaginaire et vous ne gagnerez rien du tout ! Nous devons
-          malgré tout remplir cette page alors voici des infos bidon:
-        </p>
-        <p>
-          42 rue du Donut Bleu
-          <br />
-          12345 Ville des Papillons
-          <br />
-          0-812-812-812
-        </p>
-        <p>
-          SI ça ne répond pas, c'est normal, vos chers pâtissiers ne le sont, en
-          fait, pas du tout. Ils sont malgré tout, posés devant un PC toute la
-          journée et n'aiment pas trop socialiser alors ils ont certainement
-          entendu le téléphone sonner et ont quand même décidé de l'ignorer.
-        </p>
+        <div className="background"></div>
+        <div className="text">
+          <h1 className="rainbow">La Patisserie du Dauphin arc-en-ciel </h1>
+          <p>
+            Un mariage raté? Un craquage pendant votre régime? Une tentative
+            pour impressionner votre belle-mère? Continuez à chercher, cette
+            patisserie n'est qu'imaginaire et vous ne gagnerez rien du tout !
+            Nous devons malgré tout remplir cette page alors voici des infos
+            bidon:
+          </p>
+          <p>
+            42 rue du Donut Bleu
+            <br />
+            12345 Ville des Papillons
+            <br />
+            0-812-812-812
+          </p>
+          <p>
+            SI ça ne répond pas, c'est normal, vos chers pâtissiers ne le sont,
+            en fait, pas du tout. Ils sont malgré tout, posés devant un PC toute
+            la journée et n'aiment pas trop socialiser alors ils ont
+            certainement entendu le téléphone sonner et ont quand même décidé de
+            l'ignorer.
+          </p>
+        </div>
       </div>
     </>
   );
