33ec51127805c1279bd9074a5c1c9808ad53ce03
chang10th <chang10thomas@gmail.com>
2024-01-16 10:52:20 +0100

    game_dice_face_added

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { requestPastriesWon } from "../store/gameSlice";
 import { useDispatch, useSelector } from "react-redux";
+import "../styles/game.css";
 
 function GamePage() {
   const dispatch = useDispatch();
@@ -10,10 +11,22 @@ function GamePage() {
     dispatch(requestPastriesWon(2));
     console.log(pastriesWon);
   };
+
+  const renderImages = () => {
+    const images = [];
+    for (let i = 0; i < 5; i++) {
+      images.push(<img class='de' key={i} src={`/assets/images/de${i+1}.jpg`} alt={`Dé ${i + 1}`} />);
+    }
+    return images;
+  };
+
   return (
     <>
       <h1>Game</h1>
+      {renderImages()}
+      <div className="container">
       <button onClick={handleClick}>Lancer les dés</button>
+      </div>
       <ul>
         {pastriesWon.length > 0 &&
           pastriesWon.map((pastrie, index) => (
