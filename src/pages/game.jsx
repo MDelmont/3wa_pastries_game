41c675b214c4fb7c9fb1f8977bc86e7c6764c5df
chang10th <chang10thomas@gmail.com>
2024-01-16 14:03:45 +0100

    Game dice system complete

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -7,11 +7,57 @@ function GamePage() {
   const dispatch = useDispatch();
   const { pastriesWon } = useSelector((store) => store.gameSliceReducer);
   const [diceResults, setDiceResults] = useState(Array(5).fill(1));
+  const [remainingAttempts, setRemainingAttempts] = useState(3);
 
   const handleClick = () => {
     const results = throwDices();
     setDiceResults(results);
-    //dispatch(requestPastriesWon(results));
+    const nbPastriesWon = checkVictory(results);
+
+    if (nbPastriesWon != 0) {
+      dispatch(requestPastriesWon(nbPastriesWon));
+    } else {
+      console.log("Vous n'avez rien gagné... Retentez votre chance !");
+    }
+
+    setRemainingAttempts(remainingAttempts - 1);
+      
+      if (remainingAttempts === 1) {
+        console.log("Dernier essai !");
+      }
+      
+      if (remainingAttempts === 0) {
+        console.log("Fin des essais. Désactivez le bouton ici.");
+      }
+  };
+
+  const checkVictory = (results) => {
+    // Compter le nombre d'occurrences de chaque résultat
+    const occurrences = results.reduce((acc, result) => {
+      acc[result] = (acc[result] || 0) + 1;
+      return acc;
+    }, {});
+  
+    // Vérifier les conditions de victoire
+    const values = Object.values(occurrences);
+    
+    if (values.some((count) => count === 2)) {
+      if (values.filter((count) => count === 2).length === 2) {
+        return 2; // Deux paires
+      } else {
+        return 1; // Paire
+      }
+    } else if (values.some((count) => count === 3)) {
+      if (values.some((count) => count === 2)) {
+        return 3; // Brelan avec une paire
+      } else {
+        return 2; // Brelan seul
+      }
+    } else if (values.some((count) => count === 4)) {
+      return 3; // Carré
+    }
+  
+    return 0; // Aucune condition de victoire
   };
 
   const renderImages = () => {
@@ -33,8 +79,6 @@ function GamePage() {
       const diceResult = Math.floor(Math.random() * 6) + 1;
       results.push(diceResult);
     }
-
-    console.log("Résultats des dés :", results);
     return results;
   };
 
@@ -43,7 +87,9 @@ function GamePage() {
       <div className="container">
       <h1>Game</h1>
       {renderImages()}
-      <button onClick={handleClick}>Lancer les dés</button>
+      <button onClick={handleClick} disabled={remainingAttempts === 0}>
+        Lancer les dés ({remainingAttempts} essais restants)
+      </button>
       <ul>
         {pastriesWon.length > 0 &&
           pastriesWon.map((pastrie, index) => (
