b3e9da7487e55b3f26926efe14bb2212a7528af7
chang10th <chang10thomas@gmail.com>
2024-01-16 11:36:36 +0100

    update dices images when start game

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { requestPastriesWon } from "../store/gameSlice";
 import { useDispatch, useSelector } from "react-redux";
 import "../styles/game.css";
@@ -6,27 +6,44 @@ import "../styles/game.css";
 function GamePage() {
   const dispatch = useDispatch();
   const { pastriesWon } = useSelector((store) => store.gameSliceReducer);
+  const [diceResults, setDiceResults] = useState(Array(5).fill(1));
 
   const handleClick = () => {
-    dispatch(requestPastriesWon(2));
-    console.log(pastriesWon);
+    const results = throwDices();
+    setDiceResults(results);
+    //dispatch(requestPastriesWon(results));
   };
 
   const renderImages = () => {
-    const images = [];
+    return diceResults && diceResults.map((result, index) => (
+      <img
+        className='de'
+        key={index}
+        src={`/assets/images/de${result}.jpg`}
+        alt={`Dé ${result}`}
+      />
+    ));
+  };
+
+  const throwDices = () => {
+    const results = [];
+
+    // Simuler le lancer de 5 dés
     for (let i = 0; i < 5; i++) {
-      images.push(<img class='de' key={i} src={`/assets/images/de${i+1}.jpg`} alt={`Dé ${i + 1}`} />);
+      const diceResult = Math.floor(Math.random() * 6) + 1;
+      results.push(diceResult);
     }
-    return images;
+
+    console.log("Résultats des dés :", results);
+    return results;
   };
 
   return (
     <>
+      <div className="container">
       <h1>Game</h1>
       {renderImages()}
-      <div className="container">
       <button onClick={handleClick}>Lancer les dés</button>
-      </div>
       <ul>
         {pastriesWon.length > 0 &&
           pastriesWon.map((pastrie, index) => (
@@ -35,6 +52,7 @@ function GamePage() {
             </li>
           ))}
       </ul>
+      </div>
     </>
   );
 }
